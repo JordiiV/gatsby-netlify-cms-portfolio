37c365b0c4d0019b14460f6512f70c2f9da31772
Jorge Vilardell Egea <jorge.vilardellegea@altran.es>
2018-11-16 13:08:09 +0100

    again

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,11 @@
 import CMS from 'netlify-cms'
 
+import youtube from "netlify-cms-widget-youtube";
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('blog', BlogPostPreview) 
+CMS.registerWidget("youtube", youtubeControl, youtubePreview);
